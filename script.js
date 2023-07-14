e2a1aae3ac0cce4865cbf285f8bfb85ed71908e4
Man-af <131351514+Man-af@users.noreply.github.com>
2023-07-15 00:06:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function fibonacci(num) {
 	let a=0;
 	let b=1;
 	let c=0;
-	for(let i=1; i<=num; i++)
+
+	if (num===1)
+		return a;
+
+	if (num===2)
+		return b;
+	
+	for(let i=3; i<=num; i++)
 		{
-			if (num===1)
-				return a;
-			
-			if (num===2)
-	            return b;
-			else
-			{
 				c=a+b;
 				a=b;
 				b=c;
-			}
+			
 		}
 	return c;
 }
